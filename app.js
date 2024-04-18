5c168681e15195f14b205b0d592779badc1e9a48
PashaMazur4 <pavel.mazur4@gmail.com>
2024-04-18 21:44:20 +0300

    First Hometask

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-let a = 1;
-console.log('Hello World!');
-console.log(a);
+const price = 80;
+const period = 11;
+const requestedTime = 40;
+let timeNeeded = requestedTime / 5;
+let daysNeeded = timeNeeded + 2;
+let isPossible = daysNeeded < period;
+let cost = price * requestedTime;
+
+console.log (isPossible);
+console.log (cost);
