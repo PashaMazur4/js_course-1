addee2e959f498ebc3d4d927837da73eba0afc4c
PashaMazur4 <pavel.mazur4@gmail.com>
2024-04-20 00:04:30 +0300

    Conditional statements if/else task

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const price = 80;
-const period = 11;
-const requestedTime = 40;
-let timeNeeded = requestedTime / 5;
-let daysNeeded = timeNeeded + 2;
-let isPossible = daysNeeded < period;
-let cost = price * requestedTime;
+const name = 'Peter';
+const deposit = 12000;
+const houseCost = 13500;
+const period = 24;
+const yearRate = 0.07;
 
-console.log (isPossible);
-console.log (cost);
+let withrawed = deposit * (1 + yearRate/12) ** 24;
+
+if (houseCost < withrawed) {
+    console.log(`${name} earned ${withrawed} and he can buy the house. Balance is ${withrawed - houseCost}`);
+} else {
+    console.log(`${name} earned ${withrawed} and he can't buy the house.`);
+}
